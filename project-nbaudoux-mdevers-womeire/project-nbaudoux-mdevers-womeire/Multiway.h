f9006ede716709f49c79b398ca2122a227c11e38
NBaudoux <nbaudoux@ulb.ac.be>
2018-12-23 19:02:13 +0100

    Multiway Mergesort

---
+++ project-nbaudoux-mdevers-womeire/project-nbaudoux-mdevers-womeire/Multiway.h
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "InputStream03.h"
+
+#pragma once
+class Multiway
+{
+	//By default std::priority_queue sorts according a decreasing order, std::greater<int> inverses it.
+	std::priority_queue<std::vector<int>, std::vector<std::vector<int> >, std::greater<std::vector<int> > > _queue;
+	std::vector<std::vector<int> > _content;
+	std::vector<int>_sorted;
+	std::vector<std::string> _streams;
+public:
+	Multiway(std::vector<std::string>);
+	void read();
+	void merge();
+	void showRes();
+	~Multiway();
+};
+
