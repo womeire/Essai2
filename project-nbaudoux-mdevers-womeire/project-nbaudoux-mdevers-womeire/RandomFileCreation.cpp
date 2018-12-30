c34706ff02f445ad3e16fc1f6261ad9b3e6a96a9
womeire <45068148+womeire@users.noreply.github.com>
2018-12-30 17:32:38 +0100

    Tweaked benchmarking, work in progress

--- project-nbaudoux-mdevers-womeire/project-nbaudoux-mdevers-womeire/RandomFileCreation.cpp
+++ project-nbaudoux-mdevers-womeire/project-nbaudoux-mdevers-womeire/RandomFileCreation.cpp
@@ -4,10 +4,16 @@ using namespace std;
 
 RandomFileCreation::RandomFileCreation(string filepath, int length)
 {
+	//Delete file if it existed
+	char filepathChar[_MAX_PATH];
+	strcpy_s(filepathChar, filepath.c_str());
+	std::remove(filepathChar);
+
+	//create file
 	ofstream myfile;
 	myfile.open(filepath, ios::binary | ios::out);
-
-	std::default_random_engine generator;
+	std::random_device rd;
+	std::default_random_engine generator(rd());
 	std::uniform_int_distribution<int32_t> distribution(std::numeric_limits<int32_t>::min(), std::numeric_limits<int32_t>::max());
 
 	for (int i = 0; i < length; i++)
