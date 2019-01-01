e49031b0bf8fe73ffa97b2689bc543b1d3ad4877
womeire <45068148+womeire@users.noreply.github.com>
2019-01-01 15:14:47 +0100

    New and simplified in/outstream02

--- project-nbaudoux-mdevers-womeire/project-nbaudoux-mdevers-womeire/InputStream02.cpp
+++ project-nbaudoux-mdevers-womeire/project-nbaudoux-mdevers-womeire/InputStream02.cpp
@@ -4,8 +4,6 @@
 InputStream02::InputStream02()
 {
 	filePointer = NULL;
-	fileSize_32 = 0;
-	currentPos = -1;
 }
 
 InputStream02::~InputStream02()
@@ -21,30 +19,14 @@ void InputStream02::open(string filepath)
 		return;
 	}
 
+	elementP = (int32_t*)malloc(sizeof(int32_t)*1);
+
 	char filepathChar[_MAX_PATH];
 	strcpy_s(filepathChar, filepath.c_str());
 
 	errno_t error = fopen_s(&filePointer, filepathChar, "rb");
 	if (error != 0) {
 		printf("Error opening the stream: %d \n \"%s\"\n", error, filepathChar);
-		return;
-	}
-
-	// obtain file size:
-	fseek(filePointer, 0, SEEK_END); // SEEK_END : Library implementations are allowed to not meaningfully support SEEK_END (therefore, code using it has no real standard portability).
-	fileSize_32 = ftell(filePointer) / 4; // divided by 4 because ftell returns the number of 8 bit units
-	rewind(filePointer);
-
-	// allocate memory to buffer to contain the whole file:
-	std::size_t buffSize;
-	if(sizeof(int32_t)*fileSize_32 >= _HEAP_MAXREQ)
-		//if too large error will be catched later
-		printf("File is too large");
-
-	buffer = (int32_t*)malloc(sizeof(int32_t)*fileSize_32);
-	if (buffer == NULL) {
-		printf("Error allocating memory to buffer.\n");
-		filePointer = NULL;
 		return;
 	}
 }
@@ -56,24 +38,15 @@ int32_t InputStream02::read_next()
 		return NULL;
 	}
 
-	if (currentPos < 0) {
-		fread(buffer, sizeof(int32_t), fileSize_32, filePointer);
+	if (!end_of_stream()) {
+		fread(elementP, sizeof(int32_t), 1, filePointer);
+		return *elementP;
 	}
-
-	currentPos++;
-
-	if (currentPos >= fileSize_32) {
-		return NULL;
-	}
-
-	return buffer[currentPos];
+	printf("Trying to read beyond end of file. \n");
+	return NULL;
 }
 
 bool InputStream02::end_of_stream()
 {
-	if (filePointer == NULL) {
-		printf("Invalid file or file not opened yet. \n");
-		return true; 
-	}
-	return currentPos == fileSize_32;
+	return feof(filePointer);
 }
--- project-nbaudoux-mdevers-womeire/project-nbaudoux-mdevers-womeire/OutputStream02.h
+++ project-nbaudoux-mdevers-womeire/project-nbaudoux-mdevers-womeire/OutputStream02.h
@@ -8,8 +8,9 @@ public:
 	OutputStream02();
 	~OutputStream02();
 	void create(string);
-	void write(int32_t *, int);
+	void write(int32_t);
 	void close();
 private:
 	FILE * filePointer;
+	int32_t element;
 };
